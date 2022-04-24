2d1b7e43856ca5b05619a553538809d816c610d5
CalDescent <>
2022-04-24 11:23:17 +0100

    Use correct coinbase transaction for Ravencoin

--- core/src/main/java/org/libdohj/params/RavencoinMainNetParams.java
+++ core/src/main/java/org/libdohj/params/RavencoinMainNetParams.java
@@ -95,7 +95,7 @@ public class RavencoinMainNetParams extends AbstractRavencoinParams {
             // A script containing the difficulty bits and the following message:
             //
             //   "The Times 03/Jan/2018 Bitcoin is name of the game for new generation of firms"
-            byte[] bytes = Utils.HEX.decode("04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73");
+            byte[] bytes = Utils.HEX.decode("0004ffff001d01044c4d5468652054696d65732030332f4a616e2f3230313820426974636f696e206973206e616d65206f66207468652067616d6520666f72206e65772067656e65726174696f6e206f66206669726d73");
             t.addInput(new TransactionInput(params, t, bytes));
             ByteArrayOutputStream scriptPubKeyBytes = new ByteArrayOutputStream();
             Script.writeBytes(scriptPubKeyBytes, Utils.HEX.decode("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f"));
